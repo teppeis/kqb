bf334c853297e6c9be73c6c31d9a09d42137e78c
teppeis <teppeis@gmail.com>
2021-01-16 00:21:30 +0900

    feat: implement basic query builder

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,5 +1,62 @@
-import { hello } from "..";
+import { createBuilder } from "..";
 
-test("hello returns `hello!`", () => {
-  expect(hello()).toBe("hello!");
+describe("Builder", () => {
+  test("empty build()", () => {
+    const { builder } = createBuilder();
+    expect(builder.build()).toBe("");
+  });
+
+  test("orderBy()", () => {
+    const { builder } = createBuilder();
+    expect(builder.orderBy("foo", "asc").build()).toBe("order by foo asc");
+  });
+
+  test("orderBy(): multiple", () => {
+    const { builder } = createBuilder();
+    expect(builder.orderBy("foo", "asc").orderBy("bar", "desc").build()).toBe(
+      "order by foo asc, bar desc"
+    );
+  });
+
+  test("limit()", () => {
+    const { builder } = createBuilder();
+    expect(builder.limit(100).build()).toBe("limit 100");
+  });
+
+  test("offset()", () => {
+    const { builder } = createBuilder();
+    expect(builder.offset(100).build()).toBe("offset 100");
+  });
+
+  test("where()", () => {
+    const { builder, field } = createBuilder();
+    expect(builder.where(field("foo").eq("bar")).build()).toBe(`foo = "bar"`);
+  });
+});
+
+describe("Condition", () => {
+  test("eq()", () => {
+    const { field } = createBuilder();
+    expect(field("foo").eq("bar").toQuery()).toBe(`foo = "bar"`);
+  });
+
+  test("eq(): escape double quotes", () => {
+    const { field } = createBuilder();
+    expect(field("foo").eq(`b"a"r`).toQuery()).toBe(`foo = "b\\"a\\"r"`);
+  });
+
+  test("notEq()", () => {
+    const { field } = createBuilder();
+    expect(field("foo").notEq("bar").toQuery()).toBe(`foo != "bar"`);
+  });
+
+  test("like()", () => {
+    const { field } = createBuilder();
+    expect(field("foo").like("bar").toQuery()).toBe(`foo like "bar"`);
+  });
+
+  test("notLike()", () => {
+    const { field } = createBuilder();
+    expect(field("foo").notLike("bar").toQuery()).toBe(`foo not like "bar"`);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,93 @@
-export const hello = () => "hello!";
+export const createBuilder = () => ({
+  builder: new Builder(),
+  field: conditionCreator,
+});
+
+function conditionCreator<T = string>(fieldCode: string) {
+  return new Operator<T>(fieldCode);
+}
+
+class Operator<T = string> {
+  #field: string;
+  constructor(fieldCode: string) {
+    this.#field = fieldCode;
+  }
+  eq(value: T): Condition<T> {
+    return new Condition(this.#field, "=", value);
+  }
+  notEq(value: T): Condition<T> {
+    return new Condition(this.#field, "!=", value);
+  }
+  like(value: T): Condition<T> {
+    return new Condition(this.#field, "like", value);
+  }
+  notLike(value: T): Condition<T> {
+    return new Condition(this.#field, "not like", value);
+  }
+}
+
+class Condition<T = string> {
+  #fieldCode: string;
+  #op: string;
+  #value: T;
+  constructor(fieldCode: string, op: string, value: T) {
+    this.#fieldCode = fieldCode;
+    this.#op = op;
+    this.#value = value;
+  }
+  toQuery(): string {
+    return `${this.#fieldCode} ${this.#op} "${escapeStringLiteral(String(this.#value))}"`;
+  }
+}
+
+function escapeStringLiteral(str: string): string {
+  return str.replace(/"/g, '\\"');
+}
+
+type OrderByDirection = "asc" | "desc";
+class Builder {
+  #condisions: Condition[] = [];
+  #orderByList: { field: string; direction: OrderByDirection }[] = [];
+  #limit: number | null = null;
+  #offset: number | null = null;
+
+  build(): string {
+    const buf: string[] = [];
+    if (this.#condisions.length > 0) {
+      buf.push(...this.#condisions.map((condition) => condition.toQuery()));
+    }
+    if (this.#orderByList.length > 0) {
+      buf.push("order by");
+      const fields: string[] = [];
+      this.#orderByList.forEach(({ field, direction }) => {
+        fields.push(`${field} ${direction}`);
+      });
+      buf.push(fields.join(", "));
+    }
+    if (this.#limit != null) {
+      buf.push(`limit ${this.#limit}`);
+    }
+    if (this.#offset != null) {
+      buf.push(`offset ${this.#offset}`);
+    }
+    return buf.join(" ");
+  }
+  orderBy(field: string, direction: OrderByDirection): Builder {
+    this.#orderByList.push({ field, direction });
+    return this;
+  }
+  limit(numberOfRows: number): Builder {
+    this.#limit = numberOfRows;
+    return this;
+  }
+  offset(offset: number): Builder {
+    this.#offset = offset;
+    return this;
+  }
+  where(condition: Condition): Builder {
+    this.#condisions.push(condition);
+    return this;
+  }
+}
+
+interface Condition {}
