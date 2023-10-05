6a517e5430b285cb9692a2b55bd082f154aba010
teppeis <teppeis@gmail.com>
2023-10-05 23:58:57 +0900

    chore: use eslint-config-flat-gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,7 @@
+import gitignore from "eslint-config-flat-gitignore";
 import { build } from "eslint-config-teppeis";
 
-export default await build(
-  { base: "node18", typescript: true },
-  {
-    ignores: ["dist", "coverage"],
-  },
-  {
-    files: ["**/*.test.ts"],
-    rules: { "@typescript-eslint/ban-ts-comment": "off" },
-  },
-);
+export default await build({ base: "node18", typescript: true }, gitignore(), {
+  files: ["**/*.test.ts"],
+  rules: { "@typescript-eslint/ban-ts-comment": "off" },
+});
